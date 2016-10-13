12199bbfa8eacd7f20b2c21e206575f45c031c54
Bob Ironthighs <bob.ironthighs@gmail.com>
2016-10-12 19:11:17 -0500

    Fixing up some instructions.

---
+++ enc_temp_folder/c6367382ae4076314350e5e696b330f9/NGDAGameMode.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "NGDAGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NGDA_API ANGDAGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+
+public:
+
+	ANGDAGameMode(const FObjectInitializer& ObjectInitializer);
+	virtual TSubclassOf<AGameSession> GetGameSessionClass() const override;
+
+};
